a14c336fc7f7038c2513f1a4d3475e772191acc6
nedith <nedith22@gmail.com>
2022-07-04 15:02:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,108 +1,51 @@
 /* eslint-disable no-unused-vars, no-undef, no-return-assign */
-class UI {
-  static displayBooks() {
-    const addButton = document.getElementById('add');
-    const booksUL = document.getElementById('books');
-    const { allBooks } = Storage;
+import { addBook, removeBook } from './modules/localStorage.js';
+import BookCollection from './modules/bookClass.js';
+import { insertDate } from './modules/date.js';
+import { handleLinkClick, clearFormFields } from './modules/navigation.js';
 
-    allBooks.forEach((book, index) => {
-      const li = document.createElement('li');
-      if (index % 2 === 0) {
-        li.classList.add('grey');
-      } else {
-        li.classList.add('whiteSmoke');
-      }
-      li.innerHTML = `
-               <div>
-                  <h3>"${book.title}" </h3>
-                  <span> by ${book.author}</span>
-                </div> 
-                <button data-id="${book.id}" type="button">Remove</button>
-            `;
-
-      booksUL.appendChild(li);
-    });
-
-    UI.showDigitalClock();
-    addButton.addEventListener('click', UI.addBook);
-    Navbar.show();
-    booksUL.addEventListener('click', (event) => UI.remove(event));
-  }
-
-  static addBook(event) {
-    event.preventDefault();
-
-    if (UI.formIsValid('form input')) {
-      const form = document.querySelector('form');
-
-      const id = Storage.allBooks.length + 1;
-      const newBook = {
-        id,
-        title: form.title.value,
-        author: form.author.value,
-      };
-
-      Book.save(newBook);
-      UI.appendBook(newBook);
-      UI.resetForm();
-
-      const bookList = [...document.querySelectorAll('#books li')];
-
-      bookList.forEach((item, index) => {
-        if (index % 2 === 0) {
-          item.classList.add('grey');
-        } else {
-          item.classList.add('whiteSmoke');
-        }
-      });
-    }
-  }
-
-  static formIsValid(form) {
-    const inputs = [...document.querySelectorAll(form)];
-    return inputs.every((input) => input.value.trim().length > 2);
-  }
-
-  static appendBook(newBook) {
-    const booksUL = document.getElementById('books');
-    const newBookToAppend = document.createElement('li');
-
-    newBookToAppend.innerHTML = `
-             <div>
-              <h3>"${newBook.title}" </h3>
-              <span> by ${newBook.author}</span>
-            </div>          
-            <button data-id="${newBook.id}" type="button">Remove</button>
-        `;
-
-    booksUL.appendChild(newBookToAppend);
-  }
-
-  static remove(event) {
-    if (event.target.tagName === 'BUTTON') {
-      const bookId = event.target.getAttribute('data-id');
-      Book.remove(bookId);
-
-      const listItem = event.target.parentElement;
-      listItem.remove();
-    }
-  }
-
-  static resetForm() {
-    const inputs = [...document.querySelectorAll('form input')];
-    inputs.forEach((input) => (input.value = ''));
-  }
-
-  static showDigitalClock() {
-    const showTime = () => {
-      const clock = document.getElementById('luxon_date');
-      const dt = luxon.DateTime.now();
-      const theTime = `${dt.toLocaleString(luxon.DateTime.DATE_FULL)} ${dt.toLocaleString(luxon.DateTime.TIME_WITH_SECONDS)}`;
-      clock.innerText = theTime;
-    };
-
-    setInterval(() => {
-      showTime();
-    }, 1000);
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
 }
+
+insertDate();
+
+// Display books on page load
+window.addEventListener('DOMContentLoaded', BookCollection.bookDisplay);
+
+// Add Book Event
+const formEl = document.querySelector('#book-form');
+formEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#book-name').value;
+  const author = document.querySelector('#book-author').value;
+
+  if (title !== '' && author !== '') {
+    if (e.target.classList.contains('addButton')) {
+      const newBook = new Book(title, author);
+      newBook.id = BookCollection.bookList.length + 1;
+      BookCollection.addBookUI(newBook);
+      addBook(newBook); // Add book to booklist in localStorage
+      clearFormFields();
+      formEl.submit();
+    }
+  }
+});
+
+// Remove Book from UI, localStorage and Book List
+const bookContainer = document.querySelector('.booksContainer');
+bookContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove-btn')) {
+    const bookId = e.target.parentElement.id;
+    BookCollection.removeBook(bookId);
+    removeBook(bookId);
+    e.target.parentElement.remove();
+  }
+});
+
+// Nav-links event
+document.getElementById('nav-links').addEventListener('click', handleLinkClick);
+document.getElementById('defaultOpen').click(); // Click on the list nav-link at default on page load
